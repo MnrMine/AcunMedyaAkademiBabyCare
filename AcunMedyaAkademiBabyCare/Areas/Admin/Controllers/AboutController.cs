25997d4c69f9ae64d0522ff88f0995a0fc2cff69
MnrMine <mnr_mine_agri@hotmail.com>
2025-03-27 22:20:20 +0300

    add_Admin_crud....

---
+++ AcunMedyaAkademiBabyCare/Areas/Admin/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AcunMedyaAkademiBabyCare.Areas.Admin.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
